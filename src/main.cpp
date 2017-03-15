e9d08d3580534e3ee4e73bb2d9f15c2e2df6bb40
Siddharth Atre <sidatre@gmail.com>
2017-03-14 18:39:00 -0700

    Explicitly changed OpenGL version to 4.1

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,13 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+    ofGLWindowSettings settings;
+    settings.setGLVersion(4, 1);
+    settings.width = 1024;
+    settings.height = 768;
+    
+    
+    ofCreateWindow(settings);// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
